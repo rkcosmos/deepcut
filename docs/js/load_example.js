0ccc7aa978ee60400bd05ac730bca2b28794e5c0
tulakann <tul@salweensolutions.com>
2018-04-04 11:22:55 +0700

    add load example button [#27]

---
+++ docs/js/load_example.js
@@ -0,0 +1,31 @@
+const example_list = [
+  'เราจะทำตามสัญญา ขอเวลาอีกไม่นาน\
+  แล้วแผ่นดินที่งดงามจะคืนกลับมา\
+  เราจะทำอย่างซื่อตรง ขอแค่เธอจงไว้ใจและศรัทธา\
+  แผ่นดินจะดีในไม่ช้า ขอคืนความสุขให้เธอ ประชาชน',
+
+  "'บิ๊กตู่' แจงเล็งออกกฎหมายห้ามมีกิ๊กแค่พูดเล่น บอกเป็นคนตลก \
+  ซัดสื่อสนใจแต่เรื่องขี้ไก่ 'วิษณุ'ชี้ 'ไม่มีสาระเลย' เลขาฯกฤษฎีกา\
+  เผยร่างกฎหมาย 4 ชั่วโคตรแค่ตีกรอบคนมีคู่ไม่จดทะเบียนสมรส",
+
+  "คิดว่ามันเป็นเพียงแค่ความฝัน ไม่เคยหวัง ไม่เคยคิดจริงจังอะไร \
+  แค่แอบหลงรักเธอเล่นๆ ตามลำพังข้างเดียวในหัวใจ \
+  แต่เมื่อมารู้สึกนึกอีกที เมื่อรู้ตัวอีกที ฉันก็รักจนลึกข้างใน"
+]
+
+// helper function
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function onClickLoadExample() {
+  // set val of textarea by random from example list then set height to 0
+  // and resize to scroll height (*this should be fixed more elegantly)
+  // finally focus on textarea to show cursor and trigger keyup for prediction
+  $('#message').val(function (i, text) {
+    return example_list[getRandomInt(0, example_list.length)];
+  }).height(0).height(this.scrollHeight).focus().trigger('keyup');
+}
+
